d67dd8ddc5f794f8e122d213e34d9dd48d384fbf
ZodiacalComet <ZodiacalComet@gmail.com>
2023-04-23 10:55:07 -0400

    Fix formatting of warning message in untrack

--- cli/src/subcommands/untrack.rs
+++ cli/src/subcommands/untrack.rs
@@ -9,8 +9,14 @@ pub fn untrack(story_data: &mut StoryData, Untrack { ref ids }: Untrack) {
         match story_data.shift_remove(id) {
             Some(story) => info!("{} untracked", format_story!(story)),
             None => warn!(
-                "There is no story of ID {} on the tracking list.",
+                "There is no story of ID {}{}",
                 style(id).bold(),
+                // Any created style ends with a complete reset on formatting, which resets the
+                // yellow foreground for warning messages even though we only wanted to bold the
+                // story ID.
+                // The `console` crate doesn't have a mechanism in place to change this behavior as
+                // of now, so the foreground needs to be reapplied.
+                style(" on the tracking list.").yellow()
             ),
         };
     }
